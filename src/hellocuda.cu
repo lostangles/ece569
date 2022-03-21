642feab83663b93e9999fb827c05bf55f185c8df
branjb <branjb@gmail.com>
2022-03-20 17:28:54 -0700

    Added stb_image.h

--- src/hellocuda.cu
+++ src/hellocuda.cu
@@ -1,5 +1,20 @@
 
 #include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+enum allocation_type {
+    NO_ALLOCATION, SELF_ALLOCATED, STB_ALLOCATED
+};
+
+typedef struct {
+    int width;
+    int height;
+    int channels;
+    size_t size;
+    uint8_t *data;
+    enum allocation_type allocation_;
+} Image;
 
 __global__ void cuda_hello(){
     printf("Hello World from GPU!\n");
@@ -9,10 +24,18 @@ int main() {
     cudaError_t cudaerr;
     float* deviceA;
     int result = 0;
+    Image* img = (Image*)malloc(sizeof(Image));
+    char* fname = "../images/image1.png";
+    if((img->data = stbi_load(fname, &img->width, &img->height, &img->channels, 0)) != NULL) 
+    {
+        img->size = img->width * img->height * img->channels;
+        img->allocation_ = STB_ALLOCATED;
+    }
+
+    printf("img->size:  %d img->width: %d image->height: %d img->channels: %d\n", img->size, img->width, img->height, img->channels);
 
     size_t available, total;
     cudaMemGetInfo(&available, &total);
-
 
     if(result = (cudaMalloc((void **) &deviceA, 10 * sizeof(float)) != cudaSuccess))
 	printf("Failed to malloc deviceA, errorCode:%d\n", result);
